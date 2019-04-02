3ea73c0ca38d5d7cf9987cc457a9b98ca4bda1b4
Santiago Juarez Badillo Chavez <santiago10.jbch@gmail.com>
2019-04-02 10:04:25 -0600

    Raytracer v.03
    renderTriangle  Added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Objects.Object3D;
+import Objects.Triangle;
 import Objects.Vector3;
 import Render.*;
 import Objects.Sphere;
@@ -18,10 +19,10 @@ public class Main {
 
         Scene scene01 = new Scene();
         scene01.setCamera(new Camera(new Vector3(0, 0, -8), 10,200, 160, 160, 800,800));
-        scene01.addObject(new Sphere(new Vector3(0f, 1f, 5f), 0.5f, Color.RED));
-        scene01.addObject(new Sphere(new Vector3(0f, 1f, 11f), 0.5f, Color.BLUE));
-        scene01.addObject(new Sphere(new Vector3(0f, 1f, 201f), 0.5f, Color.BLACK));
-
+        scene01.addObject(new Sphere(new Vector3(0f, 0f, 13f), 1f, Color.RED));
+        scene01.addObject(new Sphere(new Vector3(-1f, -1f, 12f), 1f, Color.BLUE));
+        scene01.addObject(new Sphere(new Vector3(-2f, 3f, 20f), 1f, Color.BLACK));
+        scene01.addObject(new Triangle(new Vector3(2f, 2f, 11f),new Vector3(4f, 2f, 11f), new Vector3(3f, 1f, 11f), Color.ORANGE));
 
         BufferedImage image = Raytracer.raytrace(scene01);
         File outputImage = new File("image.png");
---
+++ src/Objects/Triangle.java
@@ -0,0 +1,62 @@
+package Objects;
+
+import Render.Intersection;
+import Render.Ray;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+
+public class Triangle extends  Object3D{
+
+    private Vector3 position2;
+    private Vector3 position3;
+    private static double EPSILON = 0.000001;
+    public Triangle(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3, Color color){
+        super(vertex1,color);
+        setPosition2(vertex2);
+        setPosition3(vertex3);
+    }
+
+    public Vector3 getPosition2() {
+        return position2;
+    }
+
+    public void setPosition2(Vector3 position2) {
+        this.position2 = position2;
+    }
+
+    public Vector3 getPosition3() {
+        return position3;
+    }
+
+    public void setPosition3(Vector3 position3) {
+        this.position3 = position3;
+    }
+
+    public Intersection getIntersection(Ray ray) {
+        double distance = -1;
+        Vector3 normal = Vector3.ZERO();
+
+        Vector3 v2v0 = Vector3.substract(this.getPosition3(),this.getPosition());
+        Vector3 v1v0 = Vector3.substract(this.getPosition2(),this.getPosition());
+        Vector3 position = Vector3.crossProduct(ray.getDirection(),v1v0);
+        double determinant = Vector3.dotProduct(v2v0,position);
+        double invDet = 1.0/determinant;
+        Vector3 T = Vector3.substract(ray.getOrigin(),this.getPosition());
+        double u = invDet * Vector3.dotProduct(T,position);
+        Vector3 Q = Vector3.crossProduct(T,v2v0);
+        double v = invDet * Vector3.dotProduct(ray.getDirection(),Q);
+        double t = invDet * Vector3.dotProduct(Q,v1v0);
+
+        if(u < 0 || u > 1)
+            return  null;
+
+        if(v < 0 || (v + u) > (1.0 + EPSILON))
+            return  null;
+        normal = Vector3.crossProduct(v1v0,v2v0);
+        return  new Intersection( new Vector3(0f,0f,t), t, normal,this);
+
+
+
+    }
+}
